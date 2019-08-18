c18e9a3e50b39adfbcc865760c21b0ba8057b9e0
seoreo <seoreoseo@gmail.com>
2019-08-19 05:18:12 +0900

    Create Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-backgroud">
+      <img src={logo} lat="logo" />
+      <h2>Let's develop React!!!</h2>      
+    </div>
+  );
+}
+
+export default App;
